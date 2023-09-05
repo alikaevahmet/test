ce09fa3e8b3b850b7ca5b8b4eeb50f30d3bd07a0
alikaevahmet <alikaevahmet@gmail.com>
2023-09-06 00:38:23 +0600

    calc multyply

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #тут будет простой калькулятор#
 num1 = input("Введите первое число: ")
 num2 = input("Введите второе число: ")
-oper = input("Выберите и введите одну из операций + - :")
+oper = input("Выберите и введите одну из операций + - * :")
 
 num1 = int(num1)
 num2 = int(num2)
@@ -9,6 +9,8 @@ if oper == '+':
     result = num1 + num2
 elif oper == '-':
     result = num1 - num2
+elif oper == '*':
+    result = num1 * num2
 else: result = 'Я не умею в такую операцию'
 result = str(result)
 print("Ответ: " + result)
