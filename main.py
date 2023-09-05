6cc59c0b58bef58d416162a6c64c5c9e49412bf3
alikaevahmet <alikaevahmet@gmail.com>
2023-09-06 00:00:16 +0600

    calc sum

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-#пробуем ещё#
-number = input("Введите число: ")
-print("Вот ваше число: " + number)
+#тут будет простой калькулятор#
+num1 = input("Введите первое число: ")
+num2 = input("Введите второе число: ")
+#oper = input ("Выберите и введите одну из операций + - * / :")
+
+num1 = int(num1)
+num2 = int(num2)
+result = num1 + num2
+result = str(result)
+print("Вот ваше число: " + result)
 #no comments#
